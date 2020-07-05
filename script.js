25a6b96b322511e4ec1c4c9fefcd43f5b6ca387d
William Wassmann <wmwassmann@hotmail.com>
2020-07-04 17:11:37 -0700

    Second iteration, adding a timer. Though it might change depending on how I want the quiz code to loot

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const startTime = 3;
+let time = startTime * 60;
+
+const countdownEl = document.getElementById("timer");
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;    
+    time--;
+
+}
